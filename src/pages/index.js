12490911ebaf6e9330f9959fa5996867c45bcfb0
Hillzy <hillierliam37@gmail.com>
2019-06-23 23:35:51 +1000

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Products from "../components/products/Products"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 
 import "../styles/resets.css"
 
