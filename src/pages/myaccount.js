4ce0ff076b4de2c1cc630f6e8b92c84ef856f140
Hillzy <hillierliam37@gmail.com>
2019-06-30 01:07:34 +1000

    Update myaccount.js

--- src/pages/myaccount.js
+++ src/pages/myaccount.js
@@ -4,12 +4,20 @@ import Layout from "../components/Layout"
 class MyAccount extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      userName: undefined,
+    }
   }
+
+  componentDidMount() {
+    this.setState({ userName: localStorage.getItem("userName") })
+  }
+
   render() {
     return (
       <Layout>
         My Account:
-        <p>Username: {localStorage.getItem("userName")}</p>
+        <p>{this.state.userName}</p>
       </Layout>
     )
   }
